8cf0cdefa8407008fd070530662cd54c73d9b418
fairuzmn <fairuzsangar@gmail.com>
2022-04-28 09:10:14 +0700

    timestamp core barang

--- models/CoreBarang.js
+++ models/CoreBarang.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 export const CoreBarangModel = mongoose.model(
   "CoreBarang",
-  new mongoose.Schema({
-    id_vendor: String,
-    id_rincian: String,
-    nama_barang: String,
-    harga: Number,
-    img_path: String,
-  })
+  new mongoose.Schema(
+    {
+      id_vendor: String,
+      id_rincian: String,
+      nama_barang: String,
+      harga: Number,
+      img_path: String,
+    },
+    {
+      timestamps: true,
+    }
+  )
 );
